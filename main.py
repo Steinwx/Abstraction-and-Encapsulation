1ab52b9b67d935d6704312313b5fc73ec0b32f08
Steinway <adrian.montas232@gmail.com>
2023-07-31 14:49:07 +0800

    Added 1 second delay to current speed acceleration and braking.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-class Car:
-    def __init__(self, year_model, make):
-        self.__year_model = year_model
-        self.__make = make
-        self.__speed = 0
-    def accelerate(self):
-        self.__speed += 5
+import time
+from car import Car
 
-    def brake(self):
-        self.__speed -= 5
+# Program to test the Car class
+if __name__ == "__main__":
+    car = Car(2022, "Example Make")
 
-    def get_speed(self):
-        return self.__speed
+    print("Initial speed:", car.get_speed())
+
+    print("Accelerating...")
+    for _ in range(5):
+        car.accelerate()
+        print("Current speed:", car.get_speed())
+        time.sleep(1)  # Add a delay of 1 second between speed display
+
+    print("Braking...")
+    for _ in range(5):
+        car.brake()
+        print("Current speed:", car.get_speed())
+        time.sleep(1)  # Add a delay of 1 second between speed display
