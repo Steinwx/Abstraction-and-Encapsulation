8507eb6fff05d5e6ba5876bd1be277b23739f204
Steinway <adrian.montas232@gmail.com>
2023-07-31 14:59:23 +0800

    added additional methods

--- car.py
+++ car.py
@@ -15,15 +15,24 @@ class Car:
     def get_speed(self):
         return self.__speed
 
+    def set_speed(self, speed):
+        self.__speed = speed
+
     def get_make(self):
         return self.__make
+
+    def set_make(self, make):
+        self.__make = make
 
     def get_year_model(self):
         return self.__year_model
 
+    def set_year_model(self, year_model):
+        self.__year_model = year_model
+
 # Program to test the Car class
 if __name__ == "__main__":
-    car = Car(2023, "Honda")
+    car = Car(2023, "Subaru")
 
     print(f"Car Details - Make: {car.get_make()}, Year Model: {car.get_year_model()}")
     print("Initial speed:", car.get_speed())
